d45a219199e378079a471e5d6938dd13e63e1e5b
Joe Horne <joeh19@gmail.com>
2023-04-11 10:02:40 -0400

    Scheduler attempt

--- src/syncer.rs
+++ src/syncer.rs
@@ -2,17 +2,30 @@ use crate::messages::protocols::broadcast::Broadcast;
 use crate::messages::request::{Request, RequestBody};
 use crate::messages::target::Node;
 use crate::messages::target::Target;
+use clokwerk::{Scheduler, TimeUnits};
 use std::collections::HashMap;
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 
-#[derive(Debug, Default)]
+#[derive(Default)]
 pub struct Syncer {
     id: i64,
     to_sync: HashMap<i64, (Target, Target, i64)>,
 }
 
 impl Syncer {
+    pub fn new() -> &'static Mutex<Syncer> {
+        let scheduler = Scheduler::new();
+        let mut syncher = Mutex::new(Syncer {
+            ..Default::default()
+        });
+        scheduler
+            .every(10.minutes())
+            .run(|| syncher.lock().unwrap().resync());
+        &syncher
+    }
+
     pub fn send(&mut self, src: Node, dest: Node, i: i64) {
-        dbg!(&self);
         let src = Target::Node(src);
         let dest = Target::Node(dest);
         let req = Request {
@@ -32,7 +45,8 @@ impl Syncer {
         }
     }
 
-    fn resync(&mut self) {
+    fn resync(&self) {
+        dbg!(&self.to_sync.len());
         for (src, dest, i) in self.to_sync.values() {
             let req = Request {
                 src: *src,
